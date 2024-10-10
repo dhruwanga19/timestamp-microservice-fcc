19d004ef7bd342642b6e8bb83aaff67a998d167a
Dhruwang Akbari <dhruwang.akbari@gmail.com>
2024-10-09 18:45:16 -0700

    netlify support

--- index.js
+++ index.js
@@ -6,37 +6,6 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static("public"));
 
-function parseDate(dateString) {
-  if (!dateString) {
-    return new Date();
-  }
-
-  if (/^\d+$/.test(dateString)) {
-    return new Date(parseInt(dateString));
-  }
-
-  const parsedDate = new Date(dateString);
-
-  if (isNaN(parsedDate.getTime())) {
-    return null;
-  }
-
-  return parsedDate;
-}
-
-app.get("/api/:date?", (req, res) => {
-  const date = parseDate(req.params.date);
-
-  if (!date) {
-    return res.json({ error: "Invalid Date" });
-  }
-
-  res.json({
-    unix: date.getTime(),
-    utc: date.toUTCString(),
-  });
-});
-
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
---
+++ netlify/functions/api.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const serverless = require("serverless-http");
+
+const app = express();
+
+function parseDate(dateString) {
+  if (!dateString) {
+    return new Date();
+  }
+
+  if (/^\d+$/.test(dateString)) {
+    return new Date(parseInt(dateString));
+  }
+
+  const parsedDate = new Date(dateString);
+
+  if (isNaN(parsedDate.getTime())) {
+    return null;
+  }
+
+  return parsedDate;
+}
+
+app.get("/api/:date?", (req, res) => {
+  const date = parseDate(req.params.date);
+
+  if (!date) {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString(),
+  });
+});
+
+module.exports.handler = serverless(app);
